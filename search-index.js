05f83557a60414cfad74930807b20b23830002fe
Gany <Dirout@users.noreply.github.com>
2021-07-07 02:32:37 +0000

    Publish documentation to GitHub Pages

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"gany":{"doc":"","t":[5,5,5,5,5,5],"n":["add_package","create_package","drop_package","refresh_remote_packages","upgrade_packages","write_file"],"q":["gany","","","","",""],"d":["Add a package to your software installation","Package a piece of software for future distribution","Drop a package from your software installation","Refresh the local package repository with one from a …","Upgrade your local packages with newer versions held in a …","Write a file to the filesystem"],"i":[0,0,0,0,0,0],"f":[[[["argmatches",3]]],[[["argmatches",3]]],[[["argmatches",3]]],[[]],[[]],[[["str",15]]]],"p":[]}\
+}');
+if (window.initSearch) {window.initSearch(searchIndex)};
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["gany"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
