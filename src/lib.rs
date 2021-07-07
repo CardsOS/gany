12a3eb6e91081b35552a133a402515de8a4b2cd1
Gany <Dirout@users.noreply.github.com>
2021-07-07 02:31:23 +0000

    Automatically enforce Rust styleguide

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,24 @@
 use anyhow::Context;
-use std::io::Write;
-use std::io::BufWriter;
+use std::env;
 use std::fs;
 use std::fs::File;
+use std::io::BufWriter;
+use std::io::Write;
 use std::path::Path;
-use std::env;
 
 /// Add a package to your software installation
 ///
 /// # Arguments
 ///
 /// * `PACKAGE_NAME` - Path to a Mokk (required)
-pub fn add_package(matches: &clap::ArgMatches)
-{
+pub fn add_package(matches: &clap::ArgMatches) {
     let package_name = matches
-    .value_of("PACKAGE_NAME")
-    .with_context(|| "No path to a Mokk was given".to_string())
-    .unwrap();
-env::set_current_dir(package_name)
-    .with_context(|| format!("Could not read a Mokk at {}", package_name))
-    .unwrap();
+        .value_of("PACKAGE_NAME")
+        .with_context(|| "No path to a Mokk was given".to_string())
+        .unwrap();
+    env::set_current_dir(package_name)
+        .with_context(|| format!("Could not read a Mokk at {}", package_name))
+        .unwrap();
 }
 
 /// Drop a package from your software installation
@@ -27,41 +26,35 @@ env::set_current_dir(package_name)
 /// # Arguments
 ///
 /// * `PACKAGE_NAME` - Path to a Mokk (required)
-pub fn drop_package(matches: &clap::ArgMatches)
-{
+pub fn drop_package(matches: &clap::ArgMatches) {
     let package_name = matches
-    .value_of("PACKAGE_NAME")
-    .with_context(|| "No path to a Mokk was given".to_string())
-    .unwrap();
-env::set_current_dir(package_name)
-    .with_context(|| format!("Could not read a Mokk at {}", package_name))
-    .unwrap();
+        .value_of("PACKAGE_NAME")
+        .with_context(|| "No path to a Mokk was given".to_string())
+        .unwrap();
+    env::set_current_dir(package_name)
+        .with_context(|| format!("Could not read a Mokk at {}", package_name))
+        .unwrap();
 }
 
 /// Refresh the local package repository with one from a remote software distribution
-pub fn refresh_remote_packages() {
-
-}
+pub fn refresh_remote_packages() {}
 
 /// Upgrade your local packages with newer versions held in a remote software distribution
-pub fn upgrade_packages() {
-
-}
+pub fn upgrade_packages() {}
 
 /// Package a piece of software for future distribution
 ///
 /// # Arguments
 ///
 /// * `PATH` - Path to a Mokk (required)
-pub fn create_package(matches: &clap::ArgMatches)
-{
+pub fn create_package(matches: &clap::ArgMatches) {
     let path = matches
-    .value_of("PATH")
-    .with_context(|| "No path to a Mokk was given".to_string())
-    .unwrap();
-env::set_current_dir(path)
-    .with_context(|| format!("Could not read a Mokk at {}", path))
-    .unwrap();
+        .value_of("PATH")
+        .with_context(|| "No path to a Mokk was given".to_string())
+        .unwrap();
+    env::set_current_dir(path)
+        .with_context(|| format!("Could not read a Mokk at {}", path))
+        .unwrap();
 }
 
 /// Write a file to the filesystem
@@ -76,6 +69,9 @@ pub fn write_file(path: &str, data_to_write: &[u8]) {
     fs::create_dir_all(Path::new(path).parent().unwrap()).unwrap(); // Create output path, write to file
     let file = File::create(&path).unwrap(); // Create file which we will write to
     let mut buffered_writer = BufWriter::new(file); // Create a buffered writer, allowing us to modify the file we've just created
-    buffered_writer.write_all(data_to_write).with_context(|| format!("Could not write data to {}", path)).unwrap(); // Write data to file
+    buffered_writer
+        .write_all(data_to_write)
+        .with_context(|| format!("Could not write data to {}", path))
+        .unwrap(); // Write data to file
     buffered_writer.flush().unwrap(); // Empty out the data from memory after we've written to the file
 }
