c6c4c7d58a83b458c46083687dc3e7407c8bc136
Gany <Dirout@users.noreply.github.com>
2021-11-15 21:16:38 +0000

    Publish documentation to GitHub Pages

---
+++ implementors/gany/trait.Serialize.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["gany"] = [{"text":"impl <a class=\"trait\" href=\"gany/trait.Serialize.html\" title=\"trait gany::Serialize\">Serialize</a> for <a class=\"struct\" href=\"gany/struct.Package.html\" title=\"struct gany::Package\">Package</a>","synthetic":false,"types":["gany::Package"]},{"text":"impl <a class=\"trait\" href=\"gany/trait.Serialize.html\" title=\"trait gany::Serialize\">Serialize</a> for <a class=\"struct\" href=\"gany/struct.Repository.html\" title=\"struct gany::Repository\">Repository</a>","synthetic":false,"types":["gany::Repository"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
